dce99c548239cc071e8bec893a0e125ea8fddffd
Roméo Louati <romeolouati@gmail.com>
2024-03-18 12:27:43 +0100

    camera fonctionnelle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f; 
     private Mover mover;
+    private Camera mainCamera;
     private void Start()
     {
         mover = gameObject.GetComponent<Mover>();
+        mainCamera = Camera.main;
     }
 
     private void Update()
@@ -14,8 +16,27 @@ public class PlayerController : MonoBehaviour
         if (Time.timeScale > 0)
         {
             var timeScaledSpeed = speed * Time.deltaTime;
-            mover.Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized *
-                       timeScaledSpeed);
+            var direction = new Vector3();
+            
+            if (Input.GetKey(KeyCode.W))
+            {
+                direction = mainCamera.transform.forward;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                direction = mainCamera.transform.forward * -1f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                direction = mainCamera.transform.right * -1f;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                direction = mainCamera.transform.right;
+            }
+
+            direction.y = 0;
+            mover.Move(direction * timeScaledSpeed);
             //mover.LookAt(Camera.main.transform.forward);
         }
     }
